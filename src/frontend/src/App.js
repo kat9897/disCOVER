1d91285c222111fc5b8f881bbfc84079d4b07e63
Katrina Best <katrinaemily.best@mail.utoronto.ca>
2023-01-22 02:41:12 -0500

    WORKING COHERE API!!!!!!! Cleaning up now

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
- 
 import { Route, Routes } from "react-router-dom";
- 
-import Navbar from "./components/navbar";
-import PostingList from "./components/postingList";
-import Edit from "./components/edit";
-import Create from "./components/create";
+
 import LandingPage from "./screens/LandingPage";
 import ResumeAndURLPage from "./screens/ResumeAndURLPage";
 import './styles/App.css';
+import Cohere from "./components/cohere";
 
 export default class App extends Component{
   render() {
     return (
       <div className="App">
-        {/* <Navbar /> */}
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route path="/edit/:id" element={<Edit />} />
-          <Route path="/create" element={<Create />} />
+          <Route exact default path="/" element={<LandingPage />} />
           <Route path="/resume-and-url" element={<ResumeAndURLPage />} />
+          <Route path="/cohere" element={<Cohere />} />
         </Routes>
       </div>
     );
