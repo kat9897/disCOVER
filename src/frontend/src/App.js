eee5d237fdd8955dd3eeba651b7dcc76188ae69a
Tiny Griffin <chloe.stmaurice@gmail.com>
2023-01-22 03:37:23 -0500

    (maybe) fixed background problem?

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
 import { Route, Routes } from "react-router-dom";
- 
-import Navbar from "./components/navbar";
-import PostingList from "./components/postingList";
-import Edit from "./components/edit";
-import Create from "./components/create";
 import LandingPage from "./screens/LandingPage";
 import ResumeAndURLPage from "./screens/ResumeAndURLPage";
 import ResultsPage from "./screens/ResultsPage";
